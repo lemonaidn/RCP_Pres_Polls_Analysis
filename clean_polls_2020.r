5f518617b89d5dbd66563c5cf3bdbb1b0c586f5f
lemonaidn <72153806+lemonaidn@users.noreply.github.com>
2020-11-09 16:49:39 -0500

    updated data cleaner

--- clean_polls_2020.r
+++ clean_polls_2020.r
@@ -1,7 +1,10 @@
 library(tidyverse)
 library(readr)
 library(lubridate)
+library(stringr)
 polls <- read_csv("polls.csv")
+
+#index polls so we only have 2020 polls
 
 polls
 
@@ -15,6 +18,8 @@ polls_2020 = polls[2:220,] %>%
          moe)
 polls_2020
 
+#clean and convert string dates to datetime
+
 unclean_dates = polls_2020$date
 
 unclean_dates
@@ -23,11 +28,15 @@ split_dates = strsplit(unclean_dates, ' ')
 
 cleaned_dates = c()
 
+#toss out the poll's end date
+
 for (i in 1:length(split_dates)) {
   cleaned_dates[i] = split_dates[[i]][1]
 }
 
 cleaned_dates
+
+#add year so datetime conversion is possible
 
 for (i in 1:length(cleaned_dates)) {
   cleaned_dates[i] = paste0(cleaned_dates[i], '/20')
@@ -35,6 +44,7 @@ for (i in 1:length(cleaned_dates)) {
 
 cleaned_dates
 
+#convert to dates
 
 dates_as_dates = parse_date(cleaned_dates, '%m/%d/%y')
 
@@ -42,7 +52,32 @@ dates_as_dates = as_datetime(dates_as_dates)
 
 polls_2020$date = dates_as_dates
 
+#get rid of asterisks in some poll names so all polls from same pollster
+#can be grouped /plotted together later
+
 polls_2020$poll = gsub("\\*", "", polls_2020$poll)
+
+#split the sample column into separate sample size and sample type cols
+
+polls_2020 = polls_2020 %>%
+  cbind(str_split_fixed(polls_2020$sample, ' ', 2))
+
+polls_2020 = polls_2020 %>%
+  select(-'sample')
+
+polls_2020 = polls_2020 %>%
+  rename(
+    sample_size = 7,
+    sample_type = 8
+  )
+
+polls_2020
+
+#add a column that contains a numerical score based on fivethirtyeight's
+#grade-letter rating for each pollster
+#A+, A, A-, A/B, B+, B, B-, B/C, C+, C, C-, C/D, D-
+#13, 12, 11, etc etc etc
+
 
 polls_2020
 
